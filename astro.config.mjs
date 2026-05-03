869332988a1f1c4a9172aec1105d9d6a0cf71e39
Acacius03 <acacius.dev@gmail.com>
2026-05-03 22:06:14 +0800

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,74 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Quicksand",
+      cssVariable: "--font-Quicksand",
+      options: {
+        variants: [
+          {
+            weight: 300,
+            style: "normal",
+            src: ["./src/assets/fonts/Quicksand-Light.ttf"],
+          },
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/Quicksand-Regular.ttf"],
+          },
+          {
+            weight: 500,
+            style: "normal",
+            src: ["./src/assets/fonts/Quicksand-Medium.ttf"],
+          },
+          {
+            weight: 600,
+            style: "normal",
+            src: ["./src/assets/fonts/Quicksand-SemiBold.ttf"],
+          },
+          {
+            weight: 700,
+            style: "normal",
+            src: ["./src/assets/fonts/Quicksand-Bold.ttf"],
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Roboto",
+      cssVariable: "--font-Roboto",
+      options: {
+        variants: [
+          {
+            weight: 300,
+            style: "normal",
+            src: ["./src/assets/fonts/Roboto-Light.ttf"],
+          },
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/Roboto-Regular.ttf"],
+          },
+          {
+            weight: 500,
+            style: "normal",
+            src: ["./src/assets/fonts/Roboto-Medium.ttf"],
+          },
+          {
+            weight: 600,
+            style: "normal",
+            src: ["./src/assets/fonts/Roboto-SemiBold.ttf"],
+          },
+          {
+            weight: 700,
+            style: "normal",
+            src: ["./src/assets/fonts/Roboto-Bold.ttf"],
+          },
+        ],
+      },
+    },
+  ],
+});
